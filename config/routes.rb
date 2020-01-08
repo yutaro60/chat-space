eea007d1336033baa71d7841d60770334ba30d2e
yutaro60 <yasaka0407@gmail.com>
2020-01-08 19:34:58 +0900

    chat-space_group

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
- 
-
   devise_for :users
   root "messages#index"
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
